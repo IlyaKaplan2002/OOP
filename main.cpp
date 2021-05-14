34139781144a5c4522711ae616a49552dc9d427e
Ilya Kaplan <kaplan11092002@gmail.com>
2021-05-14 15:39:07 +0300

    search

--- main.cpp
+++ main.cpp
@@ -1,9 +1,21 @@
 #include <iostream>
-#include <cstdlib> // для system
+#include <cstdlib>
+#include <fstream>
 using namespace std;
 
 int main()
 {
-    cout << "Hello, world!" << endl;
+    string str;
+    cin >> "Enter a string" >> str;
+    char search;
+    cin >> "Enter a symbol, which you want to search" >> search;
+    int a = 0;
+
+    for (int i = 0; i < str.size(); i++)
+        if (str[i] == search)
+        {
+            a++;
+        }
+    cout << a << endl;
     return 0;
 }
